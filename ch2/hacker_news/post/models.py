5426992ae8a9a85579724c58dc283511e50ac093
gyungjae-ham <gyeongjae.h.dev@gmail.com>
2025-12-27 23:12:37 +0900

    add post model and migrate

--- ch2/hacker_news/post/models.py
+++ ch2/hacker_news/post/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+# 게시물
+# - 제목, 본문, 작성자 이름, 좋아요 개수, 게시물 생성시간
+
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    body = models.CharField(max_length=1024)
+    author_name = models.CharField(max_length=32)
+    points = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
