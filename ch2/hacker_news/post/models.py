f896102d14732fecd21fd53b656aa7baef5d7aba
gyungjae-ham <gyeongjae.h.dev@gmail.com>
2025-12-27 23:35:00 +0900

    add comments for django orm using lazy loading

--- ch2/hacker_news/post/models.py
+++ ch2/hacker_news/post/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 # 게시물
 # - 제목, 본문, 작성자 이름, 좋아요 개수, 게시물 생성시간
+# Django ORM은 기본적으로 Lazy Loading으로 동작한다
+# 실제 정보가 필요할 때 쿼리를 실행한다
 
 class Post(models.Model):
     title = models.CharField(max_length=128)
