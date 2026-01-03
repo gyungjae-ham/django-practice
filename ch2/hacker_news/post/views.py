2747dff649e886a1f5e674897ca841d0312168c4
gyungjae-ham <gyeongjae.h.dev@gmail.com>
2026-01-03 21:56:42 +0900

    add 3 posts and select all and join them titles

--- ch2/hacker_news/post/views.py
+++ ch2/hacker_news/post/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
 
-def index_view(request):
-    return HttpResponse("Hello, Hackers!")
+from post.models import Post
+
+
+def posts_view(request):
+    posts = Post.objects.all()
+    result = ", ".join([p.title for p in posts])
+    return HttpResponse(result)
